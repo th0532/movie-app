27e40b9db35967a222bd3c2ed96d55c97a45c9c4
th0532 <kkllpp123456@naver.com>
2020-06-09 16:54:23 +0900

    Updates

---
+++ precache-manifest.edd272ca06e45c0a5c3ba5136a1d4990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204d474e3aa1e4b40efbb35edc54815b",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "0cec744ca02e51432f9e",
+    "url": "/movie-app/static/css/main.e894d4f8.chunk.css"
+  },
+  {
+    "revision": "d17a0903073d4ea07fc9",
+    "url": "/movie-app/static/js/2.d5845d61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.d5845d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cec744ca02e51432f9e",
+    "url": "/movie-app/static/js/main.e67b7f36.chunk.js"
+  },
+  {
+    "revision": "255bee9b5e55438d0d88",
+    "url": "/movie-app/static/js/runtime-main.4f650522.js"
+  }
+]);
