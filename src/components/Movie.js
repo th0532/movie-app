c95f13ec316ee2d09742aecd74a5cde693be84e3
th0532 <kkllpp123456@naver.com>
2020-06-09 23:25:40 +0900

    redirecting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 
-function Movie({year, title, summary, poster, genres}){
+function Movie({id, year, title, summary, poster, genres}){
     return (
-    <div className="movie">
-        <img src ={poster} alt={title} title={title}/>
-        <div className="movie_data">
-            <h3 className="movie_title">{title}</h3>
-            <h5 className="movie_year">{year}</h5>
-            <ul className="movie__genres">
-                {genres.map((genre, index) =>  
-                <li key={index} className="genres_genre">
-                    {genre}
-                </li>)}
-            </ul>
-            <p className="movie_summary">{summary.slice(0, 150)}...</p>
+    <Link to = {{
+        pathname: `/movie/${id}`,
+        state:{
+            year,
+            title,
+            summary,
+            poster,
+            genres
+        }
+    }}>
+        <div className="movie">
+            <img src ={poster} alt={title} title={title}/>
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) =>  
+                    <li key={index} className="genres_genre">
+                        {genre}
+                    </li>)}
+                </ul>
+                <p className="movie_summary">{summary.slice(0, 150)}...</p>
+            </div>
         </div>
-    </div>
+    </Link>
     )
     
 }
